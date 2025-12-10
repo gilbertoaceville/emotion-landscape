d620c65db7b054dcf950a8aa68fd4f0c0678c716
gilbertoaceville <gilbertoaceville@gmail.com>
2025-12-09 22:44:54 -0600

    feat: add a create route

---
+++ app/create/page.tsx
@@ -0,0 +1,10 @@
+import { Scene } from "@/components/landscape/Scene";
+
+export const metadata = {
+  title: "Create Landscape | Emotion Landscape",
+  description: "Create your own emotional landscape",
+};
+
+export default function CreatePage() {
+  return <Scene />;
+}
